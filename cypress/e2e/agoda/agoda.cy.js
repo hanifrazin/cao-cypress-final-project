89339795945b823f4d2da8762fa78ba4d312cfc5
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2025-05-31 12:34:52 +0700

    progress agoda sampai sortBy

--- cypress/e2e/agoda/agoda.cy.js
+++ cypress/e2e/agoda/agoda.cy.js
@@ -3,6 +3,7 @@ import { DateTime } from "luxon";
 
 const input = Cypress.env("agoda");
 let nextDayDate = DateTime.fromISO(input.nextDay);
+const dataDepTime = []
 
 describe('E2E Automation Cari Tiket di Agoda', () => {
     beforeEach(() => {
@@ -16,7 +17,7 @@ describe('E2E Automation Cari Tiket di Agoda', () => {
         cy.url().should('include','#flights').then(() => {
             cy.log(`${nextDayDate.toFormat("yyyy-MM-dd")}`)
         });
-        cy.wait(5000);
+        cy.wait(10000);
 
         // Flying From
         cy.get(`#flight-origin-search-input`).click().clear().type(`${input.cityFrom}`);
@@ -26,7 +27,7 @@ describe('E2E Automation Cari Tiket di Agoda', () => {
             expect(val).not.to.be.empty;
             expect(val).to.equal(`${input.cityFrom} (${input.departureFrom})`);
         });
-        cy.wait(5000)
+
         // Flying To
         cy.get(`#flight-destination-search-input`).click().clear().type(`${input.cityArrival}`);
         cy.xpath(`//li[@data-objectid="${input.arrivalTo}"]`).click({multiple:true});
@@ -53,14 +54,29 @@ describe('E2E Automation Cari Tiket di Agoda', () => {
                 .and('include',`cabinType=${input.cabinType}`)
                 .and('include',`adults=${input.passenger}`);
         cy.xpath(`//div[@data-testid="filter-container"]//button/span[starts-with(@label,"Show all")]`).click({force:true});
-        cy.xpath(`//*[@data-component="flight-filter-item-airline"]//p`).invoke('text').then(($element) => {
-            let val = 1;
+        cy.xpath(`(//div[@data-component="flight-filter-item-airline"]/label)//p`).each(($ele,$index) => {
+            if($ele.text() === 'Malaysia Airlines'){
+                cy.xpath(`(//div[@data-component="flight-filter-item-airline"]/label)[${$index+1}]//input[@type="checkbox"]`).check();
+            }
+        });
+        cy.wait(3000);
+        cy.xpath(`//div[@data-element-name="flight-sort"]//button`).as('sortByBtn').click();
+        cy.xpath(`//div[@data-testid="floater-container"]//ul/li[4]/button`).click({force:true});
+        cy.xpath(`//h2[@data-testid="title"]`).click();
+        cy.xpath(`//div[@data-element-name="flight-sort"]//button//p[1][contains(text(),'Departure time')]`).should('be.visible');
+        cy.xpath(`//div[@data-element-name="flight-sort"]//button//p[2][contains(text(),'Earliest first')]`).should('be.visible');
+        cy.xpath(`//div[@data-testid="departure-time"]//h3`).each(($ele) => {
+            dataDepTime.push($ele.text());
+        }).then(() => {
+            cy.log(`${dataDepTime.length}`);
 
-            $element.each((index,el) => {
-                cy.log(`${index} dan Nilai ${el}`)
-            })
-        })
+        });
         
+        cy.then(() => {
+            let s = new Set(dataDepTime);
+            cy.log(`${new Array(...s).length}`);
+
+        })
         // cy.xpath(`(//div[@data-component="flight-filter-item-airline"])[7]//input[@type="checkbox"]`).check();
         // cy.xpath(`//div[@data-testid="flightCard-flight-detail"]//p[contains(text(), 'Malaysia Airlines')]`);
         // cy.then(() => {
