fa06c7aab73d9f156671c293298d5b90f9a8c471
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2025-06-03 09:18:48 +0700

    feat: ubah script ketika klik radio button

--- cypress/e2e/agoda/agoda.cy.js
+++ cypress/e2e/agoda/agoda.cy.js
@@ -63,8 +63,8 @@ describe('E2E Automation Cari Tiket di Agoda', () => {
         cy.wait(2000)
 
         cy.xpath(`//button[@data-component="flight-continue-to-addOns-button"]`).click({force:true});
-        // cy.contains('[role="radio"]', "No, thanks, I’ll risk it.").click();
-        cy.contains('p', "No, thanks, I’ll risk it.").click({ force: true });
+        cy.contains(`[role="radio"]`, `No, thanks`).click();
+
         cy.xpath(`//button[@type="submit" and @data-testid="continue-to-payment-button"]`).click();
 
         cy.wait(2000)
